cc41c1f920124b17b2eb255bef93fcbfb88e65fa
Roman <romakor.19@yandex.ru>
2023-12-11 22:21:19 +0700

    implementation of the logic of building shapes

--- FirstProject/Main.cs
+++ FirstProject/Main.cs
@@ -1,47 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Imaging;
 
 namespace Practic
 {
-    public class Scene
+    public class Program
     {
-        public void CreateBmpImage()
+        public class Scene
         {
-            Bitmap bitmap = new Bitmap(200, 200);
-
-            // Получаем объект Graphics, который позволяет рисовать на изображении
-            using (Graphics graphics = Graphics.FromImage(bitmap))
+            public Bitmap CreateBox(string filepath) //Метод для создания поля на котором мы рисуем. Он возвращает наше поле определённых размеров.
             {
-                graphics.FillRectangle(new SolidBrush(Color.Red), 10, 10, 1, 1);
+                StreamReader sr = new StreamReader(filepath);
+                string line;
+                int height = 0; int width = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(' ');
+                    if (parts.Length > 3)
+                    {
+                        if (int.TryParse(parts[0], out int x1_) &&
+                            int.TryParse(parts[1], out int y1_) &&
+                            int.TryParse(parts[2], out int x2_) &&
+                            int.TryParse(parts[3], out int y2_))
+                        {
+                            height = Math.Abs(y2_ - y1_);
+                            width = Math.Abs(x2_ - x1_);
+                        }
+                    }
+                }
+                Bitmap bitmap = new Bitmap(width, height);
+                return bitmap;
             }
-            bitmap.Save("D:\\VisualProects\\Practic\\output.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
-        }
-        public string ReadFromFile(string path)
-        {
-            string basePath = AppDomain.CurrentDomain.BaseDirectory;
-            string fullPath = Path.Combine(basePath, path);
-            if (!File.Exists(path))
+            public void CreateBoxWithFigures(string filepath) 
             {
-                throw new FileNotFoundException("файл не найден", path);
+                Bitmap bitmap = CreateBox(filepath);
+                StreamReader sr = new StreamReader(filepath);
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(' ');
+
+                    switch (parts[0])
+                    {
+                        case "point":
+                            int xP = Convert.ToInt32(parts[1]);
+                            int yP = Convert.ToInt32(parts[2]);
+
+                            Point point = new Point(xP, yP);
+                            point.DrawFigures(bitmap);
+
+                            break;
+
+                        case "rect":
+                            int x1R = Convert.ToInt32(parts[1]);
+                            int y1R = Convert.ToInt32(parts[2]);
+                            int x2R = Convert.ToInt32(parts[3]);
+                            int y2R = Convert.ToInt32(parts[4]);
+
+
+                            int heightR = Math.Abs(y2R - y1R);
+                            int widthR = Math.Abs(x2R - x1R);
+                            Rect rect = new Rect(x1R, y1R, heightR, widthR);
+                            rect.DrawFigures(bitmap);
+                            break;
+
+                        case "hline":
+                            int startHx = Convert.ToInt32(parts[1]);
+                            int startHy = Convert.ToInt32(parts[2]);
+                            int endH = Convert.ToInt32(parts[3]);
+                            HLine hLine = new HLine(startHx, startHy, endH);
+                            hLine.DrawFigures(bitmap);
+
+                            break;
+
+                        case "vline":
+                            int startVx = Convert.ToInt32(parts[1]);
+                            int startVy = Convert.ToInt32(parts[2]);
+                            int endV = Convert.ToInt32(parts[3]);
+
+                            VLine vLine = new VLine(startVx, startVy, endV);
+                            vLine.DrawFigures(bitmap);
+
+                            break;
+                    }
+                }
+                string outputPath = Path.Combine(Environment.CurrentDirectory, "output.bmp");
+                bitmap.Save(outputPath, ImageFormat.Bmp);
             }
-            string fileContent = File.ReadAllText(fullPath);
-
-            return fileContent;
         }
-    }
-
-    
-
-    public class MainClass
-    {
         public static void Main()
         {
             Scene scene = new Scene();
-            string text = scene.ReadFromFile("\\FirstProject\\TextFile1.txt");
-            Console.WriteLine(text);
-            scene.CreateBmpImage();
+            scene.CreateBoxWithFigures("TextFile1.txt");
         }
     }
 }
--- FirstProject/Object.cs
+++ FirstProject/Object.cs
@@ -2,39 +2,85 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Practic
 {
-    public class Object
+    public abstract class Object
     {
-        public int x;
-        public int y;
-        public Color color;
+        public int x { get; set; }
+        public int y { get; set; }
+        public abstract void DrawFigures(Bitmap bitmap);
+
     }
 
     public class Point : Object
     {
-        public int X;
-        public int Y;
-        public Point(int x,int y)
+        public Point(int x, int y)
         {
-            X=x; Y=y;   
+            this.x = x;
+            this.y = y;
+        }
+        public override void DrawFigures(Bitmap bitmap)
+        {
+            if ((x >= 0 && x <= bitmap.Width) && (y >= 0 && y <= bitmap.Height))
+            {
+                bitmap.SetPixel(x, y, Color.Aqua);
+            }
         }
     }
-    
 
-    //public class Rect : Point 
-    //{
+    public class Rect : Point
+    {
+        private int x1 { get; set; }
+        private int y1 { get; set; }
+        public Rect(int x, int y, int x1, int y1) : base(x, y)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+        }
+        public override void DrawFigures(Bitmap bitmap)
+        {
+            Graphics g = Graphics.FromImage(bitmap);
+            if (((x >= 0 && x <= bitmap.Width) && (x1 >= 0 && x1 <= bitmap.Width)) && ((y >= 0 && y <= bitmap.Height) && (y1 >= 0 && y1 <= bitmap.Height)))
+            {
+                g.DrawRectangle(Pens.Aqua, x, y, x1, y1);
+            }
+        }
+    }
 
-    //}
+    public class HLine : Point
+    {
+        private int length { get; set; }
+        public HLine(int x, int y, int length) : base(x, y)
+        {
+            this.length = length;
+        }
+        public override void DrawFigures(Bitmap bitmap)
+        {
+            Graphics g = Graphics.FromImage(bitmap);
+            if ((x >= 0 && x <= bitmap.Width) && (y >= 0 && y <= bitmap.Height) && (length >= 0 && length <= bitmap.Width))
+            {
+                g.DrawLine(Pens.Aqua, x, y, length, y);
+            }
+        }
+    }
 
-    //public class HLine : Point
-    //{
-
-    //}
-
-    //public class VLine : Point
-    //{
-
-    //}
+    public class VLine : Point
+    {
+        private int length { get; set; }
+        public VLine(int x, int y, int length) : base(x, y)
+        {
+            this.length = length;
+        }
+        public override void DrawFigures(Bitmap bitmap)
+        {
+            Graphics g = Graphics.FromImage(bitmap);
+            if ((x >= 0 && x <= bitmap.Width) && (y >= 0 && y <= bitmap.Height) && (length >= 0 && length <= bitmap.Height))
+            {
+                g.DrawLine(Pens.Aqua, x, y, x, length);
+            }
+        }
+    }
+ 
 }
